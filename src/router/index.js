2df4fbc004630289ed53f4365729b72025640a20
asantosmanu <manuelasantos452006@gmail.com>
2024-07-15 12:06:30 -0300

    Tabelas vue criadas

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,10 @@ import HomeView from "../views/HomeView.vue";
 import CategoriaView from "../views/CategoriaView.vue";
 import LoginView from "../views/LoginView.vue";
 import LogoutView from "../views/LogoutView.vue";
+import AcessorioView from "../views/AcessorioView.vue";
+import CorView from "../views/CorView.vue";
+import MarcaView from "../views/MarcaView.vue";
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,6 +30,21 @@ const router = createRouter({
       path: "/logout",
       name: "logout",
       component: LogoutView,
+    },
+    {
+      path: "/acessorios",
+      name: "acessorios",
+      component: AcessorioView,
+    },
+    {
+      path: "/cores",
+      name: "cores",
+      component: CorView,
+    },
+    {
+      path: "/marcas",
+      name: "marcas",
+      component: MarcaView,
     }
   ],
 });
